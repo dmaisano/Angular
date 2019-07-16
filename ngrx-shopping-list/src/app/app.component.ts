13b39a573f2f12000219ca9f7e8a919716e03051
Domenico Maisano <dev.dmaisano@gmail.com>
2019-07-15 23:22:00 -0400

    added add item functionality

--- ngrx-shopping-list/src/app/app.component.ts
+++ ngrx-shopping-list/src/app/app.component.ts
@@ -3,6 +3,8 @@ import { AppState } from './store/models/app-state.model';
 import { Store } from '@ngrx/store';
 import { ShoppingItem } from './store/models/shopping-item.model';
 import { Observable } from 'rxjs';
+import { AddItemAction } from './store/actions/shopping.actions';
+import { v4 as uuid } from 'uuid';
 
 @Component({
   selector: 'app-root',
@@ -11,10 +13,19 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent implements OnInit {
   shoppingItems$: Observable<Array<ShoppingItem>>;
+  newShoppingItem: ShoppingItem = { id: '', name: '' };
 
   constructor(private store: Store<AppState>) {}
 
   ngOnInit() {
     this.shoppingItems$ = this.store.select(store => store.shopping);
+  }
+
+  addItem() {
+    this.newShoppingItem.id = uuid;
+
+    this.store.dispatch(new AddItemAction(this.newShoppingItem));
+
+    this.newShoppingItem = { id: '', name: '' };
   }
 }
