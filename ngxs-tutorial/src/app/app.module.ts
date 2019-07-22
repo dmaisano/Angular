d6b4c15e6f92b8cf2acb05a324bf4955895b24d2
Domenico Maisano <dev.dmaisano@gmail.com>
2019-07-22 15:25:20 -0400

    removed ngrx code

--- ngxs-tutorial/src/app/app.module.ts
+++ ngxs-tutorial/src/app/app.module.ts
@@ -1,23 +1,12 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { StoreModule } from '@ngrx/store';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CreateComponent } from './create/create.component';
-import { ReadComponent } from './read/read.component';
-import { reducer } from './reducers/tutorial.reducer';
 
 @NgModule({
-  declarations: [AppComponent, ReadComponent, CreateComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    StoreModule.forRoot({
-      tutorial: reducer,
-    }),
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
